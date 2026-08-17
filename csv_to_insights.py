530700ce5cfce0344e59bd790b45c26fba9b8b25
avaortiz1507-boop <avaortiz1507@gmail.com>
2026-08-17 14:42:36 -0700

    created a separate file called results.json that stores the previous results. using results_path, i can direct that path to results.json and write the json into the file

--- csv_to_insights.py
+++ csv_to_insights.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import csv
+import json
 from statistics_calculator import calculate_stats
 
 csv_path = Path("C:/Users/avaor/Downloads/sample-superstore.csv")
+results_path = Path(__file__).with_name("results.json")
 
 try:
     with open(csv_path, mode='r', encoding='utf-8-sig', newline='') as file:
@@ -13,6 +15,10 @@ try:
         print(f"Minimum Profit: {stats['minimum']}")
         print(f"Maximum Profit: {stats['maximum']}")
         print(f"Average Profit: {stats['mean']}")
+        print(f"Results saved to {results_path}")
+        
+        with open(results_path, "w") as json_file:
+            json.dump(stats, json_file, indent=4)
 except FileNotFoundError:
     print(f"File cannot be found: {csv_path}")
 else:
