102e5ff675d30dab5e6b71b8570119665877bee6
avaortiz1507-boop <avaortiz1507@gmail.com>
2026-08-17 13:41:43 -0700

    added a numeric extraction for the Profit column. created a try and except code to catch FileNotFoundError

--- csv_to_insights.py
+++ csv_to_insights.py
@@ -3,7 +3,12 @@ import csv
 
 csv_path = Path("C:/Users/avaor/Downloads/sample-superstore.csv")
 
-with open(csv_path, mode='r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        print(row)
+try:
+    with open(csv_path, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        profits = [float(row['Profit']) for row in csv_reader]
+        for profit in profits:
+            print(profit)
+except FileNotFoundError:
+    print(f"File cannot be found: {csv_path}")
+    
